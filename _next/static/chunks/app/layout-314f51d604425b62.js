12f28f92d3b174fe381d628ba5ea5362e0ad0116
Ion Stici <ionsticidev@gmail.com>
2025-01-26 10:16:06 +0200

    gh-pages deployment

---
+++ _next/static/chunks/app/layout-314f51d604425b62.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{2226:(e,s,a)=>{Promise.resolve().then(a.t.bind(a,7215,23)),Promise.resolve().then(a.t.bind(a,347,23))},347:()=>{},7215:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_15aa97"}}},e=>{var s=s=>e(e.s=s);e.O(0,[837,441,517,358],()=>s(2226)),_N_E=e.O()}]);
